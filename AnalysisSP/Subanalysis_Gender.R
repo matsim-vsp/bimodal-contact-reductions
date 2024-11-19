2956cb59dc3c5ec4dba4c23c8194eae33f68fd62
Sydpaltra <paltra@tu-berlin.de>
2024-11-19 17:30:31 +0100

    added no. of infections and ECDF

--- AnalysisSP/Subanalysis_Gender.R
+++ AnalysisSP/Subanalysis_Gender.R
@@ -8,22 +8,19 @@ library(ggh4x)
 
 # Author: S. Paltra, contact: paltra@tu-berlin.de
 
-#In the first part of this script we compare the incidence of the respondents of the survey to the official incidence reported by RKI
-
 #raw_data <- readRDS(file = "/Users/sydney/Desktop/cleaned_data.rds")
 
 source("DataCleaningPrepForContactAnalysis.R")
 
-## ATTITUDE SCORE
 
-data_reduced <- data_reduced %>% mutate(num_c19_infs_eng = case_when(num_c19_infs == "Nie" ~ "Never",
-                                                                    num_c19_infs == "Einmal" ~ "Once",
-                                                                    num_c19_infs == "Zweimal" ~ "Twice",
-                                                                    num_c19_infs == "Dreimal" ~ "Three Times",
-                                                                    num_c19_infs == "Mehr als dreimal" ~ "More Than Three Times",
+data_reduced <- data_reduced %>% mutate(num_c19_infs_eng = case_when(num_c19_infs == "Nie" ~ "0",
+                                                                    num_c19_infs == "Einmal" ~ "1",
+                                                                    num_c19_infs == "Zweimal" ~ "2",
+                                                                    num_c19_infs == "Dreimal" ~ "3+",
+                                                                    num_c19_infs == "Mehr als dreimal" ~ "3+",
                                                                     num_c19_infs == "Ich möchte nicht antworten" ~ "I Don't Want To Answer"))                              
 
-data_reduced$num_c19_infs_eng <- factor(data_reduced$num_c19_infs_eng, levels = c("Never", "Once", "Twice", "Three Times", "More Than Three Times", "I Don't Want To Answer"))
+data_reduced$num_c19_infs_eng <- factor(data_reduced$num_c19_infs_eng, levels = c("0", "1", "2", "3+", "I Don't Want To Answer"))
 
 
 palette <- function() {
@@ -72,3 +69,62 @@ mean <- data_reduced_tidy_rel %>% filter(WhoseContacts == "Respondent") %>%
     filter(gender %in% c("female", "male")) %>% filter(TypeOfContact %in% c("Leisure", "Work")) %>%
     filter(value < 100) %>% filter(value > -150) %>%  
     filter(!is.na(TypeOfContact)) %>% group_by(TypeOfContact, gender, time) %>% summarise(mean =mean(value))
+
+
+
+data_reduced <- data_reduced %>% mutate(gender = case_when(gender == "Weiblich" ~ "female",
+                                                        gender == "Männlich" ~ "male", 
+                                                        gender == "Ich möchte nicht antworten" ~ "No Answer",
+                                                        gender == "Divers" ~ "diverse"))
+
+
+p3 <- data_reduced %>% filter(num_c19_infs_eng != "I Don't Want To Answer") %>%
+  filter(!is.na(gender)) %>% filter(gender %in% c("male", "female")) %>%
+  count(gender, num_c19_infs_eng) %>%
+  mutate(percent = 100 * n / sum(n), .by = gender) %>%
+  mutate(lci = case_when(gender == "male" ~ 386*(n/386 - 1.96*(((n/386*(1-n/386))/386)^0.5)),
+                          gender == "female" ~ 464*(n/464 - 1.96*(((n/464*(1-n/464))/464)^0.5)))) %>%#
+  mutate(lci = case_when(gender == "male" ~ 100/386*lci,
+                         gender == "female" ~ 100/464*lci)) %>%
+  mutate(lci = case_when (lci < 0 ~ 0, .default = lci)) %>%
+  mutate(uci = case_when(gender == "male" ~ 386*(n/386 + 1.96*(((n/386*(1-n/386))/386)^0.5)),
+                          gender == "female" ~ 464*(n/464 + 1.96*(((n/464*(1-n/464))/464)^0.5)))) %>%
+  mutate(uci = case_when(gender == "male" ~ 100/386*uci,
+                          gender == "female" ~ 100/464*uci)) %>%
+  ggplot(aes(num_c19_infs_eng, percent, fill = gender)) +
+  #geom_col(position = position_dodge(preserve = 'single')) +
+  geom_bar(stat = "identity", position = "dodge", width = 0.8) +
+  geom_errorbar(aes(x=num_c19_infs_eng, ymin=lci, ymax=uci, color = gender), position = position_dodge(0.8), width = 0.3, alpha=0.9, size=1.3) +
+  theme_minimal() +
+  ylab("Share [in percent]") +
+  scale_y_continuous(labels = scales::label_percent(scale = 1, accuracy = 1), breaks = c(0,25, 50)) +
+  xlab("Number of Infections") +
+  theme(text = element_text(size = 30)) +
+  theme(legend.position = "bottom", legend.title = element_blank()) +
+  #labs(fill="Behavioral Group") +
+  #theme(axis.text.x = element_text(angle=45, vjust=1, hjust=1)) +
+  scale_fill_manual(values = palette()) +
+  scale_color_manual(values = palette2()) 
+
+ggsave("NoInfections_Gender.pdf", p3, dpi = 500, w = 9, h = 9)
+ggsave("NoInfections_Gender.png", p3, dpi = 500, w = 9, h = 9)
+
+data_reduced <- data_reduced %>% mutate(date_f1_inf = case_when(is.na(date_f1_inf) ~ as.Date("3000-01-01"),
+                                        .default = as.Date(as.character(date_f1_inf)))) %>%
+                                filter(date_f1_inf != as.Date("1922-03-01")) %>%
+                                filter(date_f1_inf != as.Date("1965-06-12")) %>%
+                                filter(date_f1_inf != as.Date("2000-12-13")) %>%
+                                filter(date_f1_inf != as.Date("2019-12-21")) 
+
+p2 <- ggplot(data_reduced %>% filter(gender %in% c("female", "male")), aes(date_f1_inf, color = gender)) +
+stat_ecdf(geom="smooth", size = 2) +
+theme_minimal() +
+ylab("Empirical Cumulative \n Distribution Function") +
+xlab("Date of 1st Infection") +
+coord_cartesian(xlim=c(as.Date("2020-03-01"), as.Date("2023-08-01"))) +
+theme(text = element_text(size = 30)) +
+scale_color_manual(values = palette()) +
+theme(legend.title = element_blank(), legend.position = "bottom") 
+
+ggsave("ECDF_Gender.pdf", p2, dpi = 500, w = 9, h = 9)
+ggsave("ECDF_Gender.png", p2, dpi = 500, w = 9, h = 9)
