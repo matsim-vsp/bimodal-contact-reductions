f2de3f59de0b57ce5c4bd29b6f309d567ed5f1d7
Sydpaltra <paltra@tu-berlin.de>
2025-07-01 13:33:20 +0200

    prep for publication

--- R/groupassignmentsankey.R
+++ R/groupassignmentsankey.R
@@ -9,36 +9,34 @@ library(ggpubr)
 
 here()
 source("./R/mytheme.r")
-source("./R/DataCleaningPrepForContactAnalysis.R")
+source("./R/DataPrep.R")
 
 data_reduced_tidy_rel %>%  
-  #filter((TypeOfContact %in% c("Leisure"))) %>% 
   filter(!is.na(value)) %>% 
-  #filter(WhoseContacts %in% c("Respondent", "Household Members")) %>%
   filter(value > -150) %>% filter(value < 100) %>%    
   filter(!is.na(TypeOfContact)) %>% group_by(TypeOfContact, WhoseContacts, time) %>%
   summarise(meanRed = mean(value))
 
 ## Group Sizes When Fitting 2 Distributions to the Data
 
-# setwd("/Users/sydney/git/second-order-contacts/AnalysisSP/BimodalGroups")
-# fileNames <- Sys.glob("*.csv")
+#fileNames <- list.files("./data/BimodalGroups/", pattern = "*.csv")
+
 # 
 # dataf <- data.frame(matrix(nrow=0, ncol=4))
 # colnames(dataf) <- c("value", "group_0", "group_1", "group")
 # 
-# for(file in fileNames){
+#   for(file in fileNames){
 #   
-#   group_bel <- paste0("/Users/sydney/git/second-order-contacts/AnalysisSP/BimodalGroups/", file)
+#   group_bel <- paste0("./data/BimodalGroups/", file)
 #   
-#   group_belonging <- read_csv(group_bel)
+#   group_belonging <- read_csv(paste0("./data/BimodalGroups/", group_bel))
 #   group_belonging <- group_belonging %>% mutate(value = seq(-100,75,length.out=500))
 #   group_belonging$value <- round(group_belonging$value)
 #   group_belonging <- group_belonging %>% group_by(value) %>% 
 #     summarise(group_0 = mean(first_group), group_1 = mean(second_group))
 #   
 #   filename <- paste0(str_split(file, "csv")[[1]][1], "csv")
-#   red <- paste0("/Users/sydney/git/second-order-contacts/AnalysisSP/SecondOrderContactsPaper/DataBimodalFit/", filename)
+#   red <- paste0(getwd(), "/data/", filename)
 #   reductions <- read_csv(red)
 #   reductions <- reductions %>% dplyr::select(value)
 #   reductions$value <- round(reductions$value)
@@ -61,21 +59,19 @@ colnames(dataf) <- c("value", "group_0", "group_1", "group_2", "group")
 group_bel_long <- data.frame(matrix(nrow=0, ncol=7))
 colnames(group_bel_long) <- c("value", "ID", "group_0", "group_1", "group_2", "TypeOfContact", "time")
 
-setwd("/Users/sydney/git/second-order-contacts/AnalysisSP/TrimodalGroups")
-fileNames <- Sys.glob("*.csv")
+fileNames <- list.files("./data/TrimodalGroups/", pattern = "*.csv")
 
 for(file in fileNames){
   
-  group_bel <- paste0("/Users/sydney/git/second-order-contacts/AnalysisSP/TrimodalGroups/", file)
-  
-  group_belonging <- read_csv(group_bel)
+  group_bel <- paste0(file)
+  group_belonging <- read_csv(paste0("./data/TrimodalGroups/", group_bel))
   group_belonging <- group_belonging %>% mutate(value = seq(-100,75,length.out=500))
   group_belonging$value <- round(group_belonging$value)
   group_belonging <- group_belonging %>% group_by(value) %>% 
     summarise(group_0 = mean(first_group), group_1 = mean(second_group), group_2 = mean(third_group))
   
   filename <- paste0(str_split(file, "csv")[[1]][1], "csv")
-  red <- paste0("/Users/sydney/git/second-order-contacts/AnalysisSP/SecondOrderContactsPaper/DataBimodalFit/", filename)
+  red <- paste0(getwd(), "/data/", filename)
   reductions <- read_csv(red)
   reductions <- reductions %>%
     #filter(cond_none == "Ja") %>%
@@ -115,12 +111,11 @@ for(file in fileNames){
   group_bel_long <- rbind(group_bel_long, group_belonging)
 }
 
-#print(dataf %>% group_by(filename) %>% count(group) %>% mutate(perc = n/sum(n)), n = 100)
+print(dataf %>% group_by(filename) %>% count(group) %>% mutate(perc = n/sum(n)), n = 100)
 
 ## Group Sizes When Fitting 4 Distributions to the Data
 
-# setwd("/Users/sydney/git/second-order-contacts/AnalysisSP/QuatromodalGroups")
-# fileNames <- Sys.glob("*.csv")
+#fileNames <- list.files("./data/QuatromodalGroups/", pattern = "*.csv")
 # 
 # 
 # dataf <- data.frame(matrix(nrow=0, ncol=6))
@@ -128,7 +123,7 @@ for(file in fileNames){
 # 
 # for(file in fileNames){
 #   
-#   group_bel <- paste0("/Users/sydney/git/second-order-contacts/AnalysisSP/QuatromodalGroups/", file)
+#  group_belonging <- read_csv(paste0("./data/QuatromodalGroups/", group_bel))
 #   
 #   group_belonging <- read_csv(group_bel)
 #   group_belonging <- group_belonging %>% mutate(value = seq(-100,75,length.out=500))
@@ -137,7 +132,7 @@ for(file in fileNames){
 #     summarise(group_0 = mean(first_group), group_1 = mean(second_group), group_2 = mean(third_group), group_3=mean(fourth_group))
 #   
 #   filename <- paste0(str_split(file, "csv")[[1]][1], "csv")
-#   red <- paste0("/Users/sydney/git/second-order-contacts/AnalysisSP/SecondOrderContactsPaper/DataBimodalFit/", filename)
+#   red <- paste0(getwd(), "/data/", filename)
 #   reductions <- read_csv(red)
 #   reductions <- reductions %>% dplyr::select(value)
 #   reductions$value <- round(reductions$value)
